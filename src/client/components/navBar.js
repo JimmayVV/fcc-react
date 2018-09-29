ae37df86e15ceb6da7fc7f3c2b8c4692f963a108
Jimmy Van Veen <jimmy.van.veen@gmail.com>
2018-09-28 23:39:11 -0400

    Basic full stack node app wired up

---
+++ src/client/components/navBar.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import '../styles/navBar.css'
+
+class NavBar extends React.Component {
+  state = { name: null }
+
+  static defaultProps = {
+    links: [],
+  }
+
+  static propTypes = {
+    links: PropTypes.arrayOf(
+      PropTypes.shape({
+        href: PropTypes.string,
+        label: PropTypes.string,
+      })
+    ),
+  }
+
+  async componentDidMount() {
+    const response = await fetch('/api/getUsername')
+    const name = await response.json()
+    if (name) this.setState({ name: name.username })
+    console.log(name)
+  }
+
+  render() {
+    const { name } = this.state
+    const { links } = this.props
+    return (
+      <nav className="nav-bar">
+        <ul className="nav-bar__container">
+          <li className="nav-bar__header">
+            <Link to="/">NavBar</Link>
+          </li>
+          {links.map(link => (
+            <li className="nav-bar__link" key={`nav-bar__link${link.href}`}>
+              <Link to={link.href}>{link.label}</Link>
+            </li>
+          ))}
+          <li>{name ? `Welcome ${name}` : 'Loading Name...'}</li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+export default NavBar
--- webpack.config.js
+++ webpack.config.js
@@ -2,42 +2,50 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/client/index.js",
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: ["babel-loader"]
       },
       {
         test: /\.css$/,
         use: [
           {
-            loader: 'style-loader',
+            loader: "style-loader"
           },
           {
-            loader: 'css-loader',
-          },
-        ],
+            loader: "css-loader"
+          }
+        ]
       },
-    ],
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: "url-loader?limit=100000"
+      }
+    ]
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx'],
+    extensions: ["*", ".js", ".jsx"]
   },
   output: {
-    path: path.resolve(__dirname, 'build'),
-    publicPath: '/',
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js"
   },
   devServer: {
-    contentBase: './build',
+    contentBase: "./build",
+    port: 3000,
+    proxy: {
+      "/api": "http://localhost:8080"
+    }
   },
   plugins: [
     new HtmlWebpackPlugin({
       inject: true,
-      template: path.resolve(__dirname, 'public/index.html'),
+      template: path.resolve(__dirname, "public/index.html")
     }),
-  ],
+  ]
 };
