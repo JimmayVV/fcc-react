b1fd1f0032c458e59bb0e6b8a42227475852a223
Jimmy Van Veen <jimmy.van.veen@gmail.com>
2018-10-03 23:59:23 -0400

    Wired up Redux

--- client/src/projects/tic-tac-toe/gameBoard.js
+++ client/src/projects/tic-tac-toe/gameBoard.js
@@ -1,5 +1,150 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { playMove as _playMove, changePlayer as _changePlayer } from './actions'
 
-const GameBoard = () => <div>Board</div>
+const Cell = ({ applyClick, row, col, board }) => {
+  const icon = board[row][col]
 
-export default GameBoard
+  return (
+    <div
+      className="ticTacToe-cell"
+      onClick={() => applyClick(row, col)}
+      onKeyPress={() => applyClick(row, col)}
+      role="button"
+      tabIndex={-1}
+    >
+      <div className="ticTacToe-cell__content">{icon === null ? '' : icon.icon}</div>
+    </div>
+  )
+}
+
+Cell.propTypes = {
+  applyClick: PropTypes.func.isRequired,
+  row: PropTypes.number.isRequired,
+  col: PropTypes.number.isRequired,
+  board: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.object)).isRequired,
+}
+
+class GameBoard extends React.Component {
+  static propTypes = {
+    board: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.object)).isRequired,
+    players: PropTypes.arrayOf(
+      PropTypes.shape({
+        icon: PropTypes.string.isRequired,
+        human: PropTypes.bool.isRequired,
+      })
+    ).isRequired,
+    currentPlayer: PropTypes.shape({
+      icon: PropTypes.string.isRequired,
+      human: PropTypes.bool.isRequired,
+    }).isRequired,
+    playMove: PropTypes.func.isRequired,
+    changePlayer: PropTypes.func.isRequired,
+  }
+
+  applyClick = (row, col) => {
+    const { playMove, changePlayer, players, currentPlayer, board } = this.props
+
+    if (board[row][col] !== null) return
+
+    const newPlayer = currentPlayer.icon === players[0].icon ? players[1] : players[0]
+
+    playMove(row, col, currentPlayer)
+    changePlayer(newPlayer)
+  }
+
+  render() {
+    const { board } = this.props
+
+    return (
+      <div className="ticTacToe-board">
+        <Cell row={0} col={0} applyClick={this.applyClick} board={board} />
+        <Cell row={0} col={1} applyClick={this.applyClick} board={board} />
+        <Cell row={0} col={2} applyClick={this.applyClick} board={board} />
+        <Cell row={1} col={0} applyClick={this.applyClick} board={board} />
+        <Cell row={1} col={1} applyClick={this.applyClick} board={board} />
+        <Cell row={1} col={2} applyClick={this.applyClick} board={board} />
+        <Cell row={2} col={0} applyClick={this.applyClick} board={board} />
+        <Cell row={2} col={1} applyClick={this.applyClick} board={board} />
+        <Cell row={2} col={2} applyClick={this.applyClick} board={board} />
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    board: state.board,
+    players: state.players,
+    currentPlayer: state.currentPlayer,
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ playMove: _playMove, changePlayer: _changePlayer }, dispatch)
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(GameBoard)
+
+/* <div
+      className="ticTacToe-cell"
+      onClick={applyClick(0, 0)}
+      onKeyPress={applyClick(0, 0)}
+      role="button"
+      tabIndex={-1}
+    >
+      <div className="ticTacToe-cell__content">{board[0][0]}</div>
+    </div>
+
+    <div
+      className="ticTacToe-cell"
+      onClick={applyClick(0, 1)}
+      onKeyPress={applyClick(0, 1)}
+      role="button"
+      tabIndex={-1}
+    >
+      <div className="ticTacToe-cell__content">{board[0][1]}</div>
+    </div>
+
+    <div className="ticTacToe-cell">
+      <div className="ticTacToe-cell__content">{board[0][2]}</div>
+    </div>
+
+    <div className="ticTacToe-cell">
+      <div className="ticTacToe-cell__content">{board[1][0]}</div>
+    </div>
+
+    <div className="ticTacToe-cell">
+      <div className="ticTacToe-cell__content">{board[1][1]}</div>
+    </div>
+
+    <div className="ticTacToe-cell">
+      <div className="ticTacToe-cell__content">{board[1][2]}</div>
+    </div>
+
+    <div className="ticTacToe-cell">
+      <div className="ticTacToe-cell__content">{board[2][0]}</div>
+    </div>
+
+    <div className="ticTacToe-cell">
+      <div className="ticTacToe-cell__content">{board[2][1]}</div>
+    </div>
+
+    <div className="ticTacToe-cell">
+      <div className="ticTacToe-cell__content">{board[2][2]}</div>
+    </div>
+
+GameBoard.defaultProps = {
+  board: [[null, null, null], [null, null, null], [null, null, null]],
+  applyClick: () => {},
+}
+
+GameBoard.propTypes = {
+  board: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.string)),
+  applyClick: PropTypes.func,
+} */
--- client/src/projects/tic-tac-toe/index.js
+++ client/src/projects/tic-tac-toe/index.js
@@ -1,15 +1,70 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+
+import reducers from './reducers'
+
 import GameBoard from './gameBoard'
+import './tic-tac-toe.css'
+
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore)
+
+/*
+const Players = {
+  X: 'X',
+  O: 'O',
+}
 
 export default class TicTacToe extends Component {
-  state = {}
+
+  state = {
+    board: [[null, null, null], [null, null, null], [null, null, null]],
+    turn: Players.X,
+    computer: null,
+  }
+  
+
+  applyClick = (row, col) => {
+    this.setState(state => {
+      const { board } = state
+      let { turn } = state
+
+      if (board[row][col] === null) {
+        board[row][col] = turn
+        turn = turn === Players.X ? Players.O : Players.X
+      } else {
+        console.log('This cell has already been used')
+      }
+
+      return { board, turn }
+    })
+  }
+
+  playComputerMove = () => {}
 
   render() {
+    // const { board } = this.state
+
     return (
-      <div className="ticTacToe-container">
-        Tic Tac Toe!
-        <GameBoard />
-      </div>
+      <Provider store={createStoreWithMiddleware(reducers)}>
+        <div className="ticTacToe-container">
+          Tic Tac Toe!
+          <GameBoard />
+        </div>
+      </Provider>
     )
   }
+}
+*/
+
+export default function TicTacToe(props) {
+  return (
+    <Provider store={createStoreWithMiddleware(reducers)}>
+      <div className="ticTacToe-container">
+        Tic Tac Toe!
+        <GameBoard {...props} />
+      </div>
+    </Provider>
+  )
 }
